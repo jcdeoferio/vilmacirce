8ee1ec7ca3873e14440ba49e32a948b7ea80c53f
cedualino200 <cedualino200@149e3a1b-e557-0b7b-b086-2e100b5f900a>
2010-03-20 08:20:59 +0000

    Sending tie fighters over network

--- src/MyConnection.java
+++ src/MyConnection.java
@@ -3,17 +3,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.Socket;
+
+import com.jme.util.export.JMEExporter;
+import com.jme.util.export.JMEImporter;
+import com.jme.util.export.binary.BinaryExporter;
+import com.jme.util.export.binary.BinaryImporter;
 
 public class MyConnection {
 	
 	protected Socket socket;
-	protected ObjectOutputStream oout;
+	protected ObjectOutputStream145 oout;
 	protected ObjectInputStream oin;
+	protected JMEExporter jmee;
+	protected JMEImporter jmei;
 	
 	protected PrintWriter sout;
 	protected BufferedReader sin;
@@ -21,7 +28,10 @@ public class MyConnection {
 	public MyConnection(Socket socket) throws IOException {
 		this.socket = socket;
 		
-		this.oout = new ObjectOutputStream(socket.getOutputStream());
+		this.jmee = new BinaryExporter();
+		this.jmei = new BinaryImporter();
+		
+		this.oout = new ObjectOutputStream145(socket.getOutputStream());
 		this.oin = new ObjectInputStream(socket.getInputStream());
 		
 		OutputStream ostream = socket.getOutputStream();
@@ -32,29 +42,30 @@ public class MyConnection {
 		InputStreamReader ireader = new InputStreamReader(istream);
 		this.sin = new BufferedReader(ireader);
 
+		System.out.println("end of constructor");
 	}
 	
-	public boolean sendObject(String obj){
+	public boolean sendObject(Serializable obj){
+		System.out.println("Sending:"+obj);
+		System.out.println("Thru:"+oout);
 		try {
+//			jmee.save(sav, oout);
 			oout.writeObject(obj);
-			oout.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
+			return(false);
 		}
-		
+		System.out.println("Done sending");
 		return(true);
 	}
 	
 	public Object getObject() throws IOException{
+//		return(jmei.load(oin));
 		try {
-			
 			return(oin.readObject());
-			
 		} catch (ClassNotFoundException e) {
-			
 			e.printStackTrace();
 			return(null);
-			
 		}
 	}
 	
